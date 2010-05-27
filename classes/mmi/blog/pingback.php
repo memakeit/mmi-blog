d782c71c9d6b9faa5dcb328d618e508e119506e8
Shawn Sprehe <memakeit@gmail.com>
2010-05-26 19:35:14 -0600

    standarized no script access logic

--- classes/mmi/blog/pingback.php
+++ classes/mmi/blog/pingback.php
@@ -1,3 +1,3 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php defined('SYSPATH') or die('No direct script access.');
 
 class MMI_Blog_Pingback extends Kohana_MMI_Blog_Pingback {}
--- classes/mmi/blog/post.php
+++ classes/mmi/blog/post.php
@@ -1,3 +1,3 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php defined('SYSPATH') or die('No direct script access.');
 
 abstract class MMI_Blog_Post extends Kohana_MMI_Blog_Post {}
