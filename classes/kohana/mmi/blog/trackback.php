15a15ba475550374d6e650c0ac215b79f3a90e55
Shawn Sprehe <memakeit@gmail.com>
2010-09-05 13:38:07 -0600

    added is_duplicate and save methods

--- classes/kohana/mmi/blog/trackback.php
+++ classes/kohana/mmi/blog/trackback.php
@@ -10,6 +10,44 @@
 class Kohana_MMI_Blog_Trackback
 {
 	/**
+	 * Check if a trackback is already present for a post.
+	 * If the author parameter is a string, it represents the author's name.
+	 * If the author parameter is an array, the following keys can be used to
+	 * specify author details: name, email, url.
+	 *
+	 * @param	integer	the post id
+	 * @param	string	the content to check
+	 * @param	array	the author details
+	 * @return	boolean
+	 */
+	public static function is_duplicate($post_id, $content, $author = NULL)
+	{
+		$driver = MMI_Blog::get_config()->get('driver', MMI_Blog::DRIVER_WORDPRESS);
+		return MMI_Blog_Comment::factory($driver)->is_duplicate($post_id, $content, $author, 'trackback');
+	}
+
+	/**
+	 * Save the trackback.
+	 *
+	 * @param	integer	the post id
+	 * @param	string	the trackback page title
+	 * @param	string	the trackback URL
+	 * @return	boolean
+	 */
+	public static function save($post_id, $title, $url)
+	{
+		$driver = MMI_Blog::get_config()->get('driver', MMI_Blog::DRIVER_WORDPRESS);
+		$comment = MMI_Blog_Comment::factory($driver);
+		$comment->author = 'trackback';
+		$comment->author_url = str_replace('&', '&amp;', $url);
+		$comment->content = $title;
+		$comment->post_id = $post_id;
+		$comment->timestamp = gmdate('Y-m-d H:i:s', time());
+		$comment->type = 'trackback';
+		return $comment->save();
+	}
+
+	/**
 	 * Create a trackback instance.
 	 *
 	 * @return	MMI_Blog_Trackback
