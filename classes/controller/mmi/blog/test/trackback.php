a4fecf66ccff15bb817a19f7f473771d5666f0ab
Shawn Sprehe <memakeit@gmail.com>
2010-09-08 14:02:23 -0600

    added trackback test controller

---
+++ classes/controller/mmi/blog/test/trackback.php
@@ -0,0 +1,26 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Trackback test controller.
+ *
+ * @package		MMI Blog
+ * @author		Me Make It
+ * @copyright	(c) 2010 Me Make It
+ * @license		http://www.memakeit.com/license
+ */
+class Controller_MMI_Blog_Test_Trackback extends Controller
+{
+	/**
+	 * @var boolean turn debugging on?
+	 **/
+	public $debug = TRUE;
+
+	/**
+	 * Test trackback functionality.
+	 *
+	 * @return	void
+	 */
+	public function action_index()
+	{
+
+	}
+} // End Controller_MMI_Blog_Test_Trackback
